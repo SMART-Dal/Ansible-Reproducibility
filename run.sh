6a1f51d6f10f4c0c7dba3dc618a7bde0a53d2ec5
ghazal <sobhani.ghazal10@gmail>
2022-10-18 12:54:31 -0300

    changed run.sh to run all the models.

--- run.sh
+++ run.sh
@@ -107,6 +107,65 @@ for i in "${arr[@]}"; do
   cd "$i"/LanguageModeling/Transformer-XL/
 
 
+# Executing SSD for PyTorch and TensorFlow
+for i in "${arr[@]}"; do
+  log "Executing SSD for $i"
+  cd "$i"/Detection/SSD/
+  docker build . -t nvidia_ssd
+
+  if [ "$i" == "PyTorch" ]; then
+    if [ "$test_type" == "train" ]; then
+      log "Training SSD for PyTorch"
+      collect_energy_measurements "ssd" "$i" "$repetitions" "nvidia-docker run --rm -it --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/coco/:/coco/ --ipc=host nvidia_ssd python ./main.py --backbone resnet50 --mode training --bs 8 --epochs 1 --seed 1 --amp --data /coco"
+    else
+      log "Testing SSD for PyTorch"
+      collect_energy_measurements "ssd" "$i" "$repetitions" "nvidia-docker run --rm -it --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/coco/:/coco/ --ipc=host nvidia_ssd python ./main.py --backbone resnet50 --mode evaluation --checkpoint ./pre_train_model/epoch_*.pt --eval-batch-size 8 --epochs 1 --seed 1 --amp --data /coco"
+    fi
+    cd ../../../
+  else
+    if [ "$test_type" == "train" ]; then
+      log "Training SSD for TensorFlow"
+      collect_energy_measurements "ssd" "$i" "$repetitions" "nvidia-docker run --rm -it --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/data/:/data/coco2017_tfrecords -v ${PWD}/checkpoints/:/checkpoints --ipc=host nvidia_ssd bash ./examples/SSD320_FP16_1GPU.sh"
+    else
+      log "Testing SSD for TensorFlow"
+      collect_energy_measurements "ssd" "$i" "$repetitions" "nvidia-docker run --rm -it --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/data/:/data/coco2017_tfrecords -v ${PWD}/checkpoints/:/checkpoints --ipc=host nvidia_ssd bash ./examples/SSD320_evaluate.sh"
+    fi
+    cd ../../../
+  fi
+done
+
+# Executing GNMT for PyTorch and TensorFlow
+for i in "${arr[@]}"; do
+  log "Executing GNMT for $i"
+  cd "$i"/Translation/GNMT/
+  bash scripts/docker/build.sh
+
+  if [ "$i" == "PyTorch" ]; then
+    if [ "$test_type" == "train" ]; then
+      log "Training GNMT for PyTorch"
+      collect_energy_measurements "gnmt" "$i" "$repetitions" "docker run --gpus all --init -it --rm --network=host --ipc=host -v $PWD:/workspace/gnmt/ gnmt python3 -m torch.distributed.launch --nproc_per_node=1  train.py --seed 1 --train-global-batch-size 128"
+    else
+      log "Testing GNMT for PyTorch"
+      collect_energy_measurements "gnmt" "$i" "$repetitions" "docker run --gpus all --init -it --rm --network=host --ipc=host -v $PWD:/workspace/gnmt/ gnmt python3 translate.py --model gnmt/model_best.pth --input data/wmt16_de_en/newstest2014.en --reference data/wmt16_de_en/newstest2014.de --output /tmp/output --batch-size 128 --tables"
+    fi
+    cd ../../../
+  else
+    if [ "$test_type" == "train" ]; then
+      log "Training GNMT for TensorFlow"
+      collect_energy_measurements "gnmt" "$i" "$repetitions" "nvidia-docker run -it --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v $PWD:/workspace/gnmt/ gnmt_tf python nmt.py --output_dir=results --batch_size=128 --learning_rate=2e-3"
+    else
+      log "Testing GNMT for TensorFlow"
+      collect_energy_measurements "gnmt" "$i" "$repetitions" "nvidia-docker run -it --rm --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v $PWD:/workspace/gnmt/ gnmt_tf python nmt.py --mode=infer --output_dir=results --infer_batch_size=128 --learning_rate=2e-3"
+    fi
+    cd ../../../
+  fi
+done
+
+# Execute Transformer-XL for PyToch and TensorFlow
+for i in "${arr[@]}"; do
+  log "Executing Transofrmer-XL for $i"
+  cd "$i"/LanguageModeling/Transformer-XL/
+
   if [ "$i" == "PyTorch" ]; then
     bash pytorch/scripts/docker/build.sh
     if [ "$test_type" == "train" ]; then
@@ -125,6 +184,87 @@ for i in "${arr[@]}"; do
     else
       log "Testing Transformer-XL for TensorFlow"
       collect_energy_measurements "transformer_xl" "$i" "$repetitions" "docker run --gpus all --init -it --rm --network=host --ipc=host -v $PWD:/workspace/transformer-xl transformer-xl bash run_wt103_base.sh eval 1"
+    fi
+    cd ../../../
+  fi
+done
+
+# Executing NCF for PyTorch and TensorFlow
+for i in "${arr[@]}"; do
+  log "Executing NCF for $i"
+  cd "$i"/Recommendation/NCF/
+  docker build . -t nvidia_ncf
+  if [ "$i" == "PyTorch" ]; then
+    if [ "$test_type" == "train" ]; then
+      log "Training NCF for PyTorch"
+      collect_energy_measurements "ncf" "$i" "$repetitions" "docker run --runtime=nvidia -it --rm --ipc=host -v ${PWD}/data:/data nvidia_ncf python -m torch.distributed.launch --nproc_per_node=1 --use_env ncf.py --data ../../data/cache/ml-20m --checkpoint_dir ../../data/checkpoints/  -s 1 -e 1 -f 1 -b 64 --mode train"
+    else
+      log "Testing NCF for PyTorch"
+      collect_energy_measurements "ncf" "$i" "$repetitions" "docker run --runtime=nvidia -it --rm --ipc=host -v ${PWD}/data:/data nvidia_ncf python -m torch.distributed.launch --nproc_per_node=1 --use_env ncf.py --data ../../data/cache/ml-20m --checkpoint_dir ../../data/checkpoints/  -s 1 -e 1 -f 1 -b 64 --valid_batch_size 64 --mode test"
+    fi
+    cd ../../../
+  else
+    if [ "$test_type" == "train" ]; then
+      log "Training NCF for TensorFlow"
+      collect_energy_measurements "ncf" "$i" "$repetitions" "docker run --runtime=nvidia -it --rm --ipc=host -v ${PWD}/data:/data nvidia_ncf mpirun -np 1 --allow-run-as-root python ncf.py --amp --data ../../data/cache/ml-20m --checkpoint-dir ../../data/checkpoints/ -s 1 -e 1 -f 1 -b 64 --mode train"
+    else
+      log "Testing NCF for TensorFlow"
+      collect_energy_measurements "ncf" "$i" "$repetitions" "docker run --runtime=nvidia -it --rm --ipc=host -v ${PWD}/data:/data nvidia_ncf mpirun -np 1 --allow-run-as-root python ncf.py --amp --data ../../data/cache/ml-20m --checkpoint-dir ../../data/checkpoints/ -s 1 -e 1 -f 1 -b 64 --mode test"
+    fi
+    cd ../../../
+  fi
+done
+
+# Executing Resnet50 for PyTorch and TensorFlow
+for i in "${arr[@]}"; do
+  log "Executing ResNet50 for $i"
+  cd "$i"/Classification/ConvNets
+ # docker build . -t nvidia_rn50
+
+  if [ "$i" == "PyTorch" ]; then
+    if [ "$test_type" == "train" ]; then
+      log "Training ResNet50 for PyTorch"
+      collect_energy_measurements "resnet50" "$i" "$repetitions" "nvidia-docker run --rm -it -v ${PWD}/imagenet/:/imagenet --ipc=host nvidia_rn50 python ./launch.py --model resnet50 --precision AMP --mode benchmark_training --platform DGX1V /imagenet --epochs 4 -b 32 --prof 1851 --mixup 0.0 --workspace ./ --raport-file raport.json"
+    else
+      log "Testing ResNet50 for PyTorch"
+      collect_energy_measurements "resnet50" "$i" "$repetitions" "nvidia-docker run --rm -it -v ${PWD}/imagenet/:/imagenet --ipc=host nvidia_rn50 python ./launch.py --model resnet50 --precision AMP --mode benchmark_inference --platform DGX1V /imagenet --prof 100 --epochs 50 -b 32  --workspace ./ --raport-file inference.json"
+    fi
+    cd ../../../
+  else
+    if [ "$test_type" == "train" ]; then
+      log "Training ResNet50 for TensorFlow"
+      collect_energy_measurements "resnet50" "$i" "$repetitions" "nvidia-docker run --rm -it -v ${PWD}/imagenet/:/data/tfrecords --ipc=host nvidia_rn50 bash resnet50v1.5/training/DGX1_RN50_AMP_90E.sh"
+    else
+      log "Training ResNet50 for TensorFlow"
+      collect_energy_measurements "resnet50" "$i" "$repetitions" "nvidia-docker run --rm -it -v ${PWD}/imagenet/:/data/tfrecords --ipc=host nvidia_rn50 python main.py --mode=inference_benchmark --arch=resnet50 --num_iter=5000 --iter_unit batch --batch_size 32  --results_dir=./"
+    fi
+    cd ../../../
+  fi
+done
+
+# Executing MaskRCNN for PyTorch and TensorFlow
+for i in "${arr[@]}"; do
+  log "Executing MaskRCNN for $i"
+  cd "$i"/Segmentation/MaskRCNN/
+
+  if [ "$i" == "PyTorch" ]; then
+    cd pytorch && bash scripts/docker/build.sh && cd ..
+    if [ "$test_type" == "train" ]; then
+      log "Training MaskRCNN for PyTorch"
+      collect_energy_measurements "mask_rcnn" "$i" "$repetitions" "docker run --runtime=nvidia -v $PWD/data:/datasets/data --rm --name=maskrcnn_interactice --shm-size=10g --ulimit memlock=-1 --ulimit stack=67108864 --ipc=host -t -i nvidia_joc_maskrcnn_pt python -m torch.distributed.launch tools/train_net.py --skip-test --amp --config-file configs/e2e_mask_rcnn_R_50_FPN_1x_1GPU.yaml"
+    else
+      log "Testing MaskRCNN for PyTorch"
+      collect_energy_measurements "mask_rcnn" "$i" "$repetitions" "docker run --runtime=nvidia -v $PWD/data:/datasets/data --rm --name=maskrcnn_interactice --shm-size=10g --ulimit memlock=-1 --ulimit stack=67108864 --ipc=host -t -i nvidia_joc_maskrcnn_pt python -m torch.distributed.launch tools/test_net.py --amp --config-file configs/e2e_mask_rcnn_R_50_FPN_1x_1GPU.yaml"
+    fi
+    cd ../../../
+  else
+    nvidia-docker build -t nvidia_mrcnn_tf2 .
+    if [ "$test_type" == "train" ]; then
+      log "Training MaskRCNN for TensorFlow"
+      collect_energy_measurements "mask_rcnn" "$i" "$repetitions" "docker run --gpus 1 -it --rm --shm-size=10g --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/dataset/data/:/data -v ${PWD}/dataset/weights/:/weights nvidia_mrcnn_tf2 python main.py train --epochs 1 --steps_per_epoch 1000 --amp --train_batch_size 4"
+    else
+      log "Testing MaskRCNN for TensorFlow"
+      collect_energy_measurements "mask_rcnn" "$i" "$repetitions" "docker run --gpus 1 -it --rm --shm-size=10g --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}/dataset/data/:/data -v ${PWD}/dataset/weights/:/weights nvidia_mrcnn_tf2 python main.py infer --amp  --eval_samples 5000 --eval_batch_size 4"
     fi
     cd ../../../
   fi
