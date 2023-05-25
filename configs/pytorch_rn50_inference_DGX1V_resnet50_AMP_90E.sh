b5514f36e4ac4451bf11c381212986f0c6d91879
ghazals1121 <36522329+ghazals1121@users.noreply.github.com>
2023-05-24 23:54:03 -0300

    Delete configs directory

--- configs/pytorch_rn50_inference_DGX1V_resnet50_AMP_90E.sh
+++
@@ -1,1 +0,0 @@
-python ./launch.py --model resnet50 --precision AMP --mode benchmark_inference --platform DGX1V /imagenet --epochs 1 -b 32 --prof 100 --mixup 0.0 --workspace ./ --raport-file raport.json
