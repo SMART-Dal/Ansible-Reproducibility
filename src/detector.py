3345188077be8b3c4cb681f6d8af9264f2564cbb
sobhani <gh707451@dal.ca>
2023-06-25 16:40:25 -0300

    added multiple files operation -- provide a path to single file or a directory of files to perform the detection on.

--- src/detector.py
+++ src/detector.py
@@ -4,19 +4,43 @@ import src.smell_detection as detector
 import src.parser as parser
 from results import write_results_csv
 
+import os
+
 
 def main_method():
     try:
-        # 1- Get the input script file path from the user
-        # Make a test directory within the project directory and put the repository on that directory
-        input_file = input("Enter the path to your input file script: ")
+        # Get the input path from the user
+        input_path = input("Enter the path to your input file or directory: ")
 
-        # Check if the input file exists
-        if not os.path.isfile(input_file):
-            print("Error: The specified input file does not exist.")
+        # Check if the input path exists
+        if not os.path.exists(input_path):
+            print("Error: The specified input path does not exist.")
             return
 
-        # 3- Open playbook file and extract tasks
+        # Check if the input path is a file
+        if os.path.isfile(input_path):
+            process_file(input_path)
+        else:
+            # Input path is a directory
+            files = get_files_from_directory(input_path)
+            if not files:
+                print("Error: No files found in the specified directory.")
+                return
+            for file in files:
+                process_file(file)
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+
+def process_file(input_file):
+    try:
+        # Check if the input file exists
+        if not os.path.isfile(input_file):
+            print(f"Error: The specified input file '{input_file}' does not exist.")
+            return
+
+        # Open playbook file and extract tasks
         with open(input_file) as f:
             # Get the parsed tasks as a dictionary
             tasks = parser.get_parsed_tasks(input_file=f)
@@ -26,9 +50,17 @@ def main_method():
                 output_tasks, new_output_tasks = detector.detect_smells(task, task_number, input_file)
                 write_results_csv(output_tasks, new_output_tasks, input_file)
     except FileNotFoundError:
-        print("Error: File not found.")
+        print(f"Error: File '{input_file}' not found.")
     except Exception as e:
-        print("An error occurred:", str(e))
+        print(f"An error occurred while processing file '{input_file}':", str(e))
+
+
+def get_files_from_directory(directory):
+    files = []
+    for root, _, filenames in os.walk(directory):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
 
 
 if __name__ == "__main__":
