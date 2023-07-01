26bae35e652c14573b8ba5e3bd61dc04c6af2e40
sobhani <gh707451@dal.ca>
2023-07-01 19:06:21 -0300

    added the urls to download the repositories.

--- src/script_extractor.py
+++ src/script_extractor.py
@@ -5,7 +5,15 @@ headers = {"Authorization": "Bearer <GITHUB_ACCESS_TOKEN>"}
 search_term = "ansible"
 
 # Search for Ansible scripts on GitHub using the GitHub API
-url = f"https://api.github.com/search/code?q=ansible+in:file+language:yaml+language:json+{search_term}"
+url = f"https://github.com/oracle/oci-ansible-collection/tree/master/samples"
+url2 = 'https://galaxy.ansible.com/search?deprecated=false&tags=system&keywords=&order_by=-relevance'
+url3 = 'https://galaxy.ansible.com/search?deprecated=false&tags=networking&keywords=&order_by=-relevance'
+url4 = 'https://galaxy.ansible.com/search?deprecated=false&tags=cloud&keywords=&order_by=-relevance'
+url5 = 'https://galaxy.ansible.com/search?deprecated=false&tags=database&keywords=&order_by=-relevance'
+url6 = 'https://galaxy.ansible.com/search?deprecated=false&tags=monitoring&keywords=&order_by=-relevance'
+url7 = 'https://galaxy.ansible.com/search?deprecated=false&tags=security&keywords=&order_by=-relevance'
+url8 = 'https://galaxy.ansible.com/search?deprecated=false&tags=web&keywords=&order_by=-relevance'
+
 response = requests.get(url, headers=headers)
 
 try:
