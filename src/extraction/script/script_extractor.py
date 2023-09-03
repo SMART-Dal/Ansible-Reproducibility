52259349d3382fa0543b7940f39dc31522309566
sobhani <gh707451@dal.ca>
2023-09-02 23:19:18 -0300

    development and system repositories for empirical study.

--- src/extraction/script/script_extractor.py
+++ src/extraction/script/script_extractor.py
@@ -67,6 +67,6 @@ def clone_github_yml_files(links):
 
 if __name__ == '__main__':
     # Provide a list of GitHub repository links
-    repository_links = ['https://github.com/avanov/ansible-galaxy-pyenv.git']
+    repository_links = ['https://github.com/Comcast/ansible-sdkman.git']
 
     clone_github_yml_files(repository_links)
