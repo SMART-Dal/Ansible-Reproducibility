a76374ba6af77b3533dde6a5ca6bc79f5ab6aee6
sobhani <gh707451@dal.ca>
2023-09-03 21:47:56 -0300

    finished the web repositories for empirical studies

--- src/extraction/script/script_extractor.py
+++ src/extraction/script/script_extractor.py
@@ -67,6 +67,6 @@ def clone_github_yml_files(links):
 
 if __name__ == '__main__':
     # Provide a list of GitHub repository links
-    repository_links = ['https://github.com/weareinteractive/ansible-nginx.git']
+    repository_links = ['https://github.com/lukasic/ansible-role-multi-php.git']
 
     clone_github_yml_files(repository_links)
