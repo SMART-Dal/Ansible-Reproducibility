875fd12a99f24c60b086aec92ebd328e649fd051
sobhani <gh707451@dal.ca>
2023-09-05 20:26:32 -0300

    finished cloud repositrories for empirical study

--- src/extraction/script/script_extractor.py
+++ src/extraction/script/script_extractor.py
@@ -67,6 +67,6 @@ def clone_github_yml_files(links):
 
 if __name__ == '__main__':
     # Provide a list of GitHub repository links
-    repository_links = ['https://github.com/diodonfrost/ansible-role-amazon-codedeploy.git']
+    repository_links = ['https://github.com/serverscom/serverscom-ansible']
 
     clone_github_yml_files(repository_links)
