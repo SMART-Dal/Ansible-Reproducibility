3b65335e6e856891bf131eae6bcd9d4e9ebcfd89
sobhani <gh707451@dal.ca>
2023-09-05 14:04:45 -0300

    added packaging repos for empirical study

--- src/extraction/script/script_extractor.py
+++ src/extraction/script/script_extractor.py
@@ -67,6 +67,6 @@ def clone_github_yml_files(links):
 
 if __name__ == '__main__':
     # Provide a list of GitHub repository links
-    repository_links = ['https://github.com/Tirasa/SyncopeAnsible.git']
+    repository_links = ['https://github.com/diodonfrost/ansible-role-amazon-codedeploy.git']
 
     clone_github_yml_files(repository_links)
