181b65c52b6853b7028dab1c0ff9d3751fd37671
sobhani <gh707451@dal.ca>
2023-09-09 17:28:47 -0300

    updating repositories lists

--- src/extraction/script/script_extractor.py
+++ src/extraction/script/script_extractor.py
@@ -67,5 +67,5 @@ def clone_github_yml_files(links):
 
 if __name__ == '__main__':
     # Provide a list of GitHub repository links
-    repository_links = ['https://github.com/ansible-middleware.git']
+    repository_links = ['https://github.com/ovirt/ovirt-ansible-collection']
     clone_github_yml_files(repository_links)
