551c28459496233c3a0c86286b30207294414146
Tushar Sharma <000.tushar@gmail.com>
2024-08-01 10:31:00 -0300

    remove old files and structure

--- src/run_detector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# List of file or repositories absolute paths to be passed as arguments
-file_paths=()
-
-# Iterate over each file path and run the command
-for file_path in "${file_paths[@]}"; do
-    python detector.py "$file_path"
-done
